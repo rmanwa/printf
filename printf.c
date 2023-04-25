4a2b3f2c4a9a8d45fbbc769b1bb7683b9764e9f7
leone-nyaga <leonenyaga@gmail.com>
2023-04-25 12:25:53 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- printf.c
+++ printf.c
@@ -7,53 +7,55 @@
  *
  * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, count = 0;
-    char c, *s;
+	va_list args;
+	int i, count = 0;
+	char c, *s;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            putchar(format[i]);
-            count++;
-        }
-        else
-        {
-            switch (format[++i])
-            {
-                case 'c':
-                    c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                case 's':
-                    s = va_arg(args, char *);
-                    while (*s)
-                    {
-                        putchar(*s++);
-                        count++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(format[i]);
-                    count += 2;
-                    break;
-            }
-        }
-    }
+	for (i = 0; format[1] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			switch (format[++i])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					putchar(c);
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					while (*s)
+					{
+						putchar(*s++);
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return (count);
 }
+
 
